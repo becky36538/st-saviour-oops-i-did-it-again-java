b15eb1d883c3075cd41b410874fd1da3412455e6
bmc2425 <recheriyan@stsaviour.org>
2026-06-11 00:07:55 -0400

    oops i did it again

--- whoops/DessertTest.java
+++ whoops/DessertTest.java
@@ -1,10 +1,10 @@
 package whoops;
 
-public class DessertTest {
-    public static void main(String[] args) {
-        Cookies cookie = new Cookies("Chocolate Chip Cookie");
+public class DessertTest { //test class used to run program
+    public static void main(String[] args) { // starting point
+        Cookies cookie = new Cookies("Chocolate Chip Cookie"); // creates cookies object called cookie, name is passed to constructor
 
-        System.out.println(cookie.display());
-        System.out.println(cookie.bake());
+        System.out.println(cookie.display()); //calls display from desserts, prints result
+        System.out.println(cookie.bake()); // calls bake from bakedDesserts, prints result
     }
 }
